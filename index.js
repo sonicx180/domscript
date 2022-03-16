f77fa2b6f6135d2c4100473dc0b0e65145cf2e22
sonicx180 <83236320+sonicx180@users.noreply.github.com>
2022-03-15 19:45:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-
+var $ = (tag,id,tclass,all) => {
+  if(id == true) {
+    document.getElementById(tag);
+    
+  }
+  if (tclass == true) {
+    document.getElementsByClassName(tag);
+  }
+  if(all == true) {
+    document.querySelectorAll(tag);
+    
+  }
+  
+}
